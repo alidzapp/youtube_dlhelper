f987b07789ea99680fbe7ac65fbe6a39d62e1887
Sascha Manns <Sascha.Manns@directbox.com>
2013-12-28 17:17:35 +0100

    Completed coveralls specfile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
 require 'coveralls'
 Coveralls.wear!
 
+# SimpleCov Formatter Task
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '.yardoc'
+end
+task :default => :test
+
