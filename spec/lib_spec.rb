2096ea13bd91c41e356b2bf4daeba28eea9d8ed2
Sascha Manns <Sascha.Manns@directbox.com>
2014-01-04 23:59:37 +0100

    updated libs checker

--- spec/lib_spec.rb
+++ spec/lib_spec.rb
@@ -3,6 +3,7 @@ require 'dir'
 require 'fileutils2'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'youtube_dlhelper/libs'
+@tempfile = 'Crystallize___Lindsey_Stirling__Dubstep_Violin_Original_Song_'
 
 #set :environment, :test
 
@@ -27,20 +28,18 @@ describe 'Download Checker' do
   it 'shoud download a file from Youtube' do
       puts 'Entering Check: Download Checker'
       YoutubeDlhelperLibs::Downloader.get('http://www.youtube.com/watch?v=aHjpOzsQ9YI')
-      tempfile = 'Crystallize___Lindsey_Stirling__Dubstep_Violin_Original_Song_'
-      testfile = Dir.glob('*.mp4')
-      testfile.equal? "#{tempfile}.mp4"
+      File.exists?("#{@tempfile}.mp4")
       puts 'Test passed'
+
   end
 end
 
 describe 'Transcoding Checker' do
   it 'should converts a file from Youtube' do
     puts 'Entering Test: Transcoding Checker'
-    tempfile = 'Crystallize___Lindsey_Stirling__Dubstep_Violin_Original_Song_'
-    YoutubeDlhelperLibs::Ripper.rip("#{tempfile}")
-    testfile = Dir.glob('*.mp3')
-    testfile.equal? "#{tempfile}.mp3"
+    @tempfile = 'Crystallize___Lindsey_Stirling__Dubstep_Violin_Original_Song_'
+    YoutubeDlhelperLibs::Ripper.rip("#{@tempfile}")
+    File.exists?("#{@tempfile}.mp3")
     puts 'Test passed...'
   end
 end
@@ -48,9 +47,9 @@ end
 describe 'Cleanup Routine' do
   it 'Should cleanup all downloaded and generated files' do
     require 'fileutils2'
-    tempfile = 'Crystallize___Lindsey_Stirling__Dubstep_Violin_Original_Song_'
-    File.delete("#{tempfile}.mp4")
-    File.delete("#{tempfile}.m4a")
-    File.delete("#{tempfile}.mp3")
+    @tempfile = 'Crystallize___Lindsey_Stirling__Dubstep_Violin_Original_Song_'
+    File.delete("#{@tempfile}.mp4")
+    File.delete("#{@tempfile}.m4a")
+    File.delete("#{@tempfile}.mp3")
   end
 end
